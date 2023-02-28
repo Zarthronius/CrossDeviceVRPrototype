65ca369e565ec9eb42121c9ae8488aaa2f7006b1
Zac Allen <zacallen12@yahoo.co.uk>
2023-02-28 22:00:03 +0000

    Networked object render now toggles on button press

--- CrossDeviceVRPrototype/Assets/NetworkRenderToggle.cs
+++ CrossDeviceVRPrototype/Assets/NetworkRenderToggle.cs
@@ -2,32 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Ubiq.Messaging;
+using UnityEngine.UIElements;
+using Toggle = UnityEngine.UI.Toggle;
 
 public class NetworkRenderToggle : MonoBehaviour
 {
     NetworkContext context;
     public Renderer r;
     
+    //public Toggle renderToggle;
+
     // Start is called before the first frame update
     void Start()
     {
         r = GetComponent<Renderer>();
         context = NetworkScene.Register(this);
+        //NetworkId networkIdSquare = new NetworkId(1001); //
+        context = new NetworkContext(); //
     }
 
-    bool visible;
+    bool lastRendered;
     
     // Update is called once per frame
     void Update()
     {
-        visible = r.enabled;
+        if (lastRendered != r.enabled)
+        {
+            lastRendered = r.enabled;
+            Debug.Log(lastRendered);
+
+            /*context.SendJson(new Message()
+            {
+                isRendered = lastRendered
+            });*/
+        }
+    }
+
+    /*public void IsVisible()
+    {
+        r.enabled = !r.enabled;
+        //visible = r.enabled;
         Debug.Log(visible);
-        
+
         context.SendJson(new Message()
         {
-            isRendered = visible
+            isRendered = r.enabled
         });
-    }
+        //Debug.Log("visible value sent: "+visible);
+    }*/
 
     private struct Message
     {
@@ -38,5 +60,7 @@ public class NetworkRenderToggle : MonoBehaviour
     {
         var m = message.FromJson<Message>();
         r.enabled = m.isRendered;
+        lastRendered = r.enabled;
+        Debug.Log(r.enabled);
     }
 }
