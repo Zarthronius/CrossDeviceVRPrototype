31af5e253deb67c7120cf2d0b771e61fa2a3e5bf
Zac Allen <zacallen12@yahoo.co.uk>
2023-04-23 22:14:07 +0100

    Changes and optimisations

--- CrossDeviceMobilePrototype/Assets/myScripts/myNewRoomButton.cs
+++ CrossDeviceMobilePrototype/Assets/myScripts/myNewRoomButton.cs
@@ -1,11 +1,9 @@
-//TODO: Joincode is being taken BEFORE room is initialised. ONLY take join code AFTER initialisation.
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Ubiq.Rooms;
-using UnityEditor.PackageManager;
+//using UnityEditor.PackageManager;
 using System.Threading.Tasks;
 
 namespace Ubiq.Samples
@@ -15,18 +13,11 @@ namespace Ubiq.Samples
         public SocialMenu mainMenu;
         public Text nameText;
         public bool publish;
-        //public string joinCode;
-		
-		//public Text Joincode;
 
         // Expected to be called by a UI element
         public async void NewRoom()
         {
-			//nameText.text = "testRoom";
             await createRoom();
-            //joinCode = mainMenu.roomClient.Room.JoinCode;
-            //Debug.Log("Shouldn't be first");
-            //Debug.Log("joinCode2: " + joinCode);
             Debug.Log(mainMenu.roomClient.Room.JoinCode);
         }
 
@@ -38,47 +29,6 @@ namespace Ubiq.Samples
                     name: nameText.text,
                     publish: publish);
             });
-            //Debug.Log(mainMenu.roomClient.Room.JoinCode);
-            //Debug.Log("should be first");
         }
     }
 }
-
-
-
-/*using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using Ubiq.Rooms;
-using UnityEditor.PackageManager;
-
-//
-using System.Threading.Tasks;
-
-namespace Ubiq.Samples
-{
-    public class NewRoomButton : MonoBehaviour
-    {
-        public SocialMenu mainMenu;
-        public Text nameText;
-        public bool publish;
-        public string joinCode;
-
-        // Expected to be called by a UI element
-        public async void NewRoom()
-        {
-            await Task.Run(() => createRoom());
-            joinCode = mainMenu.roomClient.Room.JoinCode;
-            Debug.Log("joinCode: " + joinCode);
-
-        }
-
-        void createRoom()
-        {
-            mainMenu.roomClient.Join(
-                name: nameText.text,
-                publish: publish);
-        }
-    }
-}*/
